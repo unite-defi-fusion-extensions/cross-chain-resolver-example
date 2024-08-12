b9bd54bd31347cc5c8ecb4e9402d6fb4407fd561
Vladimir Borovik <v.borovik@1inch.io>
2024-08-12 12:31:44 +0700

    feat: base test with single fill

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
 import oneInchEslintConfig from "@1inch/eslint-config";
 
-export default oneInchEslintConfig;
+export default [...oneInchEslintConfig, {
+    rules: {
+        "no-console": "off",
+    },
+    files: ['tests/**/**']
+}];
--- tests/config.ts
+++ tests/config.ts
@@ -1,34 +1,47 @@
-import {z} from "zod";
-import * as process from "node:process";
+import {z} from 'zod'
 import Sdk from '@1inch/cross-chain-sdk'
+import * as process from 'node:process'
 
 const ConfigSchema = z.object({
     SRC_CHAIN_RPC: z.string().url(),
     DST_CHAIN_RPC: z.string().url(),
+    SRC_CHAIN_CREATE_FORK: z.coerce.boolean().default(true),
+    DST_CHAIN_CREATE_FORK: z.coerce.boolean().default(true)
 })
 
 const fromEnv = ConfigSchema.parse(process.env)
-
 
 export const config = {
     chain: {
         source: {
             chainId: Sdk.NetworkEnum.ETHEREUM,
             url: fromEnv.SRC_CHAIN_RPC,
-            escrowFactoryAddress: Sdk.Address.fromBigInt(1n).toString(),
+            createFork: fromEnv.SRC_CHAIN_CREATE_FORK,
             limitOrderProtocol: '0x111111125421ca6dc452d289314280a0f8842a65',
             wrappedNative: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-            ownerPrivateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+            ownerPrivateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+            tokens: {
+                USDC: {
+                    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+                    donor: '0x4B16c5dE96EB2117bBE5fd171E4d203624B014aa'
+                }
+            }
         },
         destination: {
-            chainId: Sdk.NetworkEnum.ARBITRUM,
+            chainId: Sdk.NetworkEnum.BINANCE,
             url: fromEnv.DST_CHAIN_RPC,
-            escrowFactoryAddress: Sdk.Address.fromBigInt(1n).toString(),
+            createFork: fromEnv.DST_CHAIN_CREATE_FORK,
             limitOrderProtocol: '0x111111125421ca6dc452d289314280a0f8842a65',
-            wrappedNative: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
-            ownerPrivateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+            wrappedNative: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
+            ownerPrivateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+            tokens: {
+                USDC: {
+                    address: '0x8965349fb649a33a30cbfda057d8ec2c48abe2a2',
+                    donor: '0x4188663a85C92EEa35b5AD3AA5cA7CeB237C6fe9'
+                }
+            }
         }
     }
-}
+} as const
 
-export type ChainConfig = typeof config.chain['source' | 'destination']
+export type ChainConfig = (typeof config.chain)['source' | 'destination']
